14b3146f01b6bc8ab46f8af3883c60bcf72b69f2
jumpei yamamoto <jumpei.yamamoto@gmail.com>
2019-01-03 01:21:24 +0900

    add package_generator

--- generator/class_generator.py
+++ generator/class_generator.py
@@ -5,12 +5,15 @@ from typing import Optional
 class ClassGenerator:
 
     def __init__(self, current_dir: Path, package_name: str, class_name: str, method_num: int,
-                 prev_class_name: Optional[str]):
+                 prev_class: Optional['ClassGenerator']):
         self.dir = current_dir
         self.package_dir = current_dir / package_name
         self.class_name = class_name
         self.method_num = method_num
-        self.prev_class_name = prev_class_name
+        self.prev_class = prev_class
+
+    def get_import_name(self):
+        return self.get_package_name() + "." + self.class_name
 
     def get_package_name(self):
         ls = list(self.package_dir.parts)
@@ -25,12 +28,14 @@ class ClassGenerator:
 
     def generate_class_text(self, file):
         print("package {package_name}".format(package_name=self.get_package_name()), file=file)
+        if self.prev_class is not None and self.prev_class.get_package_name() != self.get_package_name():
+            print("import {import_name}".format(import_name=self.prev_class.get_import_name()))
         print("class {class_name} {{".format(class_name=self.class_name), file=file)
-        if self.prev_class_name is None:
+        if self.prev_class is None:
             print("    fun method0() = Unit", file=file)
         else:
             print("    fun method0() = {prev_class_name}().method{method_num}()".format(
-                prev_class_name=self.prev_class_name, method_num=self.method_num), file=file)
+                prev_class_name=self.prev_class.class_name, method_num=self.method_num), file=file)
         for i in range(self.method_num):
             print("    fun method{num}() = method{prev}()".format(num=i + 1, prev=i), file=file)
 
@@ -40,6 +45,7 @@ class ClassGenerator:
 if __name__ == '__main__':
     generator = ClassGenerator(
         Path("../SingleModuleApp/app/src/main/java/com/github/yamamotoj/singlemoduleapp"), "foo",
-        "foo", 5, "bar")
+        "foo", 5, None)
     print(generator.get_package_name())
     print(generator.generate_class_text(None))
+    generator.write_class_file()
---
+++ generator/package_generator.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from class_generator import ClassGenerator
+
+
+class PackageGenerator:
+
+    def __init__(self, parent_dir: Path, package_prefix: str, class_num: int,
+                 class_prefix: str, method_num: int):
+        self.parent_dir = parent_dir
+        self.package_prefix = package_prefix
+        self.class_num = class_num
+        self.class_prefix = class_prefix
+        self.method_num = method_num
+
+    def split_list(self, l, n):
+        """
+        リストをサブリストに分割する
+        :param l: リスト
+        :param n: サブリストの要素数
+        :return:
+        """
+        for idx in range(0, len(l), n):
+            yield l[idx:idx + n]
+
+    def generate_classes(self):
+        prev_class = None
+        for package_index, package in enumerate(self.split_list(range(self.class_num), 100)):
+            package_name = "{}{:0=2}".format(self.package_prefix, package_index)
+            for class_index in package:
+                class_name = "{}{:0=5}".format(self.class_prefix, class_index)
+                class_generator = ClassGenerator(self.parent_dir, package_name, class_name, 5,
+                                                 prev_class)
+                class_generator.write_class_file()
+                prev_class = class_generator
+
+
+if __name__ == '__main__':
+    generator = PackageGenerator(
+        Path("../SingleModuleApp/app/src/main/java/com/github/yamamotoj/singlemoduleapp"), "foo",
+        10000, "bar", 5)
+    generator.generate_classes()
